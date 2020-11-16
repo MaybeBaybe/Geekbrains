b0a6165de3822b1819ac117d8990e659aa78e69b
MaybeBaybe <mkraskinm@gmail.com>
2020-11-16 22:34:26 +0300

    1st task

--- README.md
+++
@@ -1,1 +0,0 @@
-# Geekbrains-python2020
--- hw4/funcFor1.py
+++
@@ -1,5 +0,0 @@
-def salary(working_out, rate_per_hour, bonus):
-    try:
-        return (working_out*rate_per_hour) + bonus
-    except TypeError:
-        return
